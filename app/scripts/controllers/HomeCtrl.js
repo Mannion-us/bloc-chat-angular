73b7256df55912468e2ed75a2d14f594aa8deffc
Thomas Mannion <tmannion@gmail.com>
2017-06-09 11:40:36 -0400

    Test of 'app' still failing; 'push' and contacting mentor for help...

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -6,3 +6,7 @@
 		.module('blocChatAngular')
 		.controller('HomeCtrl', [HomeCtrl]);
 })();
+
+/*<!--	Still unable to get a "Successful" test to produce the "Home Page" 
+'view' pushing this branch to gitHub 'as is' and attempting to contact a 
+mentor to assist me as directed by curriculum...-->*/
